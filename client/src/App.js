d7001fdf1c78488bd04ec153d00ca92f4a152543
unknown <janf@gmatc.matc.edu>
2022-10-26 09:49:10 -0500

    Started Activity 8

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
 import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './App.css';
 import Register from './components/Register/Register';
-import Login from './components/Login';
+import Login from './components/Login/Login';
 
 class App extends React.Component {
   state = {
@@ -25,31 +25,31 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>GoodThings</h1>
-          <ul>
-          <li>
-          <Link to="/">Home</Link>
-          </li>
-          <li>
-          <Link to="/register">Register</Link>
-          </li>
-          <li>
-          <Link to="/login">Login</Link>
-          </li>
-          </ul>
-        </header>
-        <main>
-          <Route exact path="/">
-          {this.state.data}
-          </Route>
-          <Switch>
-          <Route exact path="/register" component={Register}/>
-          <Route exact path="/login" component={Login}/>
-          </Switch>
-        </main>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <h1>GoodThings</h1>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </header>
+          <main>
+            <Route exact path="/">
+              {this.state.data}
+            </Route>
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </main>
+        </div>
       </Router>
     );
   }
