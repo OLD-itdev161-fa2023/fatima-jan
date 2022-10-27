04258380dde055f0c2412386211d7e459f05b1d9
janf319 <janf@gmatc.matc.edu>
2022-10-27 08:46:32 -0500

    Completed Acitivity 8

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
-
+//login component front end
 const Login = ({ authenticateUser }) => {
   let history = useHistory();
   const [userData, setUserData] = useState({
